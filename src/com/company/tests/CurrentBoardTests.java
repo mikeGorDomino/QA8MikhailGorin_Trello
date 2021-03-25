1b9342fa1f0462cedd7246eb1f82b876143e25c5
mikeGorDomino <m.gorin.airlife@gmail.com>
2021-03-25 05:25:26 +0200

    HomeWork for CurrentBoardHelper and CurrentBoardTests
    Signed-off-by: mikeGorDomino <m.gorin.airlife@gmail.com>

--- src/com/company/tests/CurrentBoardTests.java
+++ src/com/company/tests/CurrentBoardTests.java
@@ -1,6 +1,7 @@
 package com.company.tests;
 
 import helpers.BoardsPageHelper;
+import helpers.CurrentBoardHelper;
 import helpers.LoginPageHelper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -13,12 +14,14 @@ public class CurrentBoardTests extends TestBase{
 
     LoginPageHelper loginPage;
     BoardsPageHelper boardsPage;
+    CurrentBoardHelper listPage;
 
     @BeforeMethod
     public void initTests() {
-        // ---------Press login button  ---
+        // ---------Press login button and enter to the Board QA Haifa8 ---
         loginPage = new LoginPageHelper(driver);
         boardsPage = new BoardsPageHelper(driver);
+        listPage = new CurrentBoardHelper(driver);
         loginPage.openLoginPage();
         loginPage.waitUntilPageIsLoaded();
         loginPage.enterLoginPasswordAttl(LOGIN, PASSWORD);
@@ -29,134 +32,68 @@ public class CurrentBoardTests extends TestBase{
     }
 
     @Test
-    public void createNewList() throws InterruptedException{
-       // ---- count numbers of list in the board ----
-        int listBefore = driver.findElements(By.cssSelector(".list-header")).size();
-
-        WebElement addListButton = driver.findElement(By.xpath("//span[@class='placeholder']/.."));
-        addListButton.click();
-        //Thread.sleep(2000);
-        waitUntilElementIsClickable(By.xpath("//span[@class='placeholder']/.."),5);
-        WebElement titleListField = driver.findElement(By.xpath("//input[@name = 'name']"));
-        fillField(titleListField, "QWDFKL");
-        WebElement submitButton = driver.findElement(By.xpath("//input[@value = 'Add list']"));
-        submitButton.click();
-        WebElement cancelEditList = driver.findElement(By.cssSelector(".js-cancel-edit"));
-        cancelEditList.click();
-
+    public void createNewList() {
+       // ---- count numbers of list in the board before add ----
+        int listBeforeCreate = listPage.countLists();
+        listPage.pushButtonAddList();
+        listPage.getNameNewList();
+        listPage.finishAddList();
         // ------- count quantity lists in the board after create newList -----
-        int listAfterCreate = driver.findElements(By.cssSelector(".list-header")).size();
-
-       Assert.assertEquals(listBefore+1, listAfterCreate, "The quantity lists after" +
-                " newCreate is equals quantity before newCreate + 1");
+       int listAfterCreate = listPage.countLists();
+       Assert.assertEquals(listBeforeCreate+1, listAfterCreate, "The quantity lists after" +
+               " newCreate is equals quantity before newCreate + 1");
 
     }
 
    @Test
-    public void changeNameList() throws InterruptedException{
+    public void changeNameList() {
 
-        WebElement addList = driver.findElement(By.xpath("//span[@class = 'placeholder']"));
-        if(addList.getText().equals("Add a list")){
-            addList.click();
-
-            WebElement newNameList = driver.findElement(By.xpath("//input[@class = 'list-name-input']"));
-            fillField(newNameList, "Gorin1-QA8");
-            //Thread.sleep(2000);
-            waitUntilElementIsClickable(By.xpath("//input[@class = 'list-name-input']"),10);
-
-            WebElement saveNameList = driver.findElement(By.cssSelector("input[value='Add List']"));
-            saveNameList.click();
-            //Thread.sleep(2000);
-            waitUntilElementIsClickable(By.cssSelector("input[value='Add list']"),10);
-
-            WebElement closeCreateNewList = driver.findElement(By.xpath("//a[@class = 'icon-lg icon-close" +
-                    " dark-hover js-cancel-edit']"));
-            closeCreateNewList.click();
-            //Thread.sleep(2000);
-            waitUntilElementIsClickable(By.xpath("//a[@class = 'icon-lg icon-close" +
-                    " dark-hover js-cancel-edit']"), 10);
+        if (listPage.checkAvailabilList().equals("Add a list")) {
+            listPage.pushButtonAddList();
+            listPage.getNameNewList();
+            listPage.finishAddList();
         }
+       listPage.clickOnHeader();
+       String oldName = listPage.nameOfHeaderList();
 
-        // ----- I find index of the last list in Board --------------
-        int lastList = driver.findElements(By.cssSelector(".list-header")).size()-1;
+       listPage.getNewName();
+       String newName = listPage.nameOfHeaderList();
 
-        //-------- Click on the header--------------
-        WebElement lastHeader = driver.findElements(By.cssSelector(".list-header")).get(lastList);
-        lastHeader.click();
-       //Thread.sleep(2000);
-       waitUntilElementIsClickable(By.cssSelector(".list-header"),10);
-
-        //------- Change the header -----------------
-        String newHeader = "newGorinList";
-        WebElement lastNameList = driver.findElements(By.cssSelector(".js-list-name-input")).get(lastList);
-        lastNameList.sendKeys(newHeader);
-        //Thread.sleep(2000);
-       waitUntilElementIsClickable(By.cssSelector(".js-list-name-input"),10);
-        lastNameList.sendKeys(Keys.ENTER);
-        //Thread.sleep(2000);
-       waitUntilElementIsClickable(By.cssSelector(".js-list-name-input"),10);
-        driver.navigate().refresh();
-        Thread.sleep(2000);
-
-
-        lastHeader = driver.findElements(By.cssSelector(".list-header")).get(lastList);
-
-        Assert.assertEquals(lastHeader.getText(), newHeader);
+       Assert.assertNotEquals(oldName, newName);
     }
 
     @Test
-    public void addNewCardAtList() throws InterruptedException{
+    public void addNewCardAtList() {
+        int cardsBefore = listPage.countCard();
 
-        WebElement addList = driver.findElement(By.xpath("//span[@class = 'placeholder']"));
-        if(addList.getText().equals("Add a list")) {
-            createNewList();
-        }
-        WebElement addCard = driver.findElement(By.xpath("//a[@class='open-card-composer" +
-                " js-open-card-composer']"));
-        if(addCard.getText().equals("Add a card")) {
-            WebElement newCard = driver.findElement(By.xpath("//span[@class = 'js-add-a-card']"));
-            newCard.click();
-            //Thread.sleep(2000);
-            waitUntilElementIsClickable(By.xpath("//span[@class = 'js-add-a-card']"),10);
-            String name = "FirstCard";
-            WebElement cardName = driver.findElement(By.cssSelector(".js-card-title"));
-            cardName.sendKeys(name);
-            //Thread.sleep(2000);
-            waitUntilElementIsClickable(By.cssSelector(".js-card-title"),10);
-            cardName.sendKeys(Keys.ENTER);
-            //Thread.sleep(2000);
-            waitUntilElementIsClickable(By.cssSelector(".js-card-title"),10);
+        if (listPage.checkAvailabilList().equals("Add a list")) {
+            listPage.pushButtonAddList();
+            listPage.getNameNewList();
+            listPage.finishAddList();
         }
         else {
-            WebElement addAnotherCard = driver.findElement(By.xpath("//span[@class='js-add-another-card']"));
-            addAnotherCard.click();
-            String newName = "NoFirstCard1234";
-            WebElement cardName = driver.findElement(By.cssSelector(".js-card-title"));
-            cardName.sendKeys(newName);
-            //Thread.sleep(2000);
-            waitUntilElementIsClickable(By.cssSelector(".js-card-title"), 10);
-            cardName.sendKeys(Keys.ENTER);
-            //Thread.sleep(2000);
-            waitUntilElementIsClickable(By.cssSelector(".js-card-title"), 10);
+            listPage.getNameNewCard();
         }
+
+        int cardsAfterCreate = listPage.countCard();
+        Assert.assertEquals(cardsBefore+1,cardsAfterCreate,
+                "The quantity of cards after adding is not equal to cards before adding plus one");
 
     }
 
     @Test
-    public void deleteList() throws InterruptedException{
-        WebElement addList = driver.findElement(By.xpath("//span[@class = 'placeholder']"));
-        if(addList.getText().equals("Add a list")) {
-            createNewList();
+    public void deleteList() {
+
+        if (listPage.checkAvailabilList().equals("Add a list")) {
+            listPage.pushButtonAddList();
+            listPage.getNameNewList();
+            listPage.finishAddList();
         }
+        int listBeforeDelete = listPage.countLists();
+        listPage.listDelete();
+        int listAfterDelete = listPage.countLists();
 
-        WebElement listMenu = driver.findElement(By.cssSelector(".list-header-extras-menu"));
-        listMenu.click();
-        //Thread.sleep(2000);
-        waitUntilElementIsClickable(By.cssSelector(".list-header-extras-menu"),10);
-        WebElement archiveTheList = driver.findElement(By.cssSelector(".js-close-list"));
-        archiveTheList.click();
-        //Thread.sleep(2000);
-        waitUntilElementIsClickable(By.cssSelector(".js-close-list"),10);
-
+        Assert.assertEquals(listBeforeDelete-1, listAfterDelete,
+                "The quantity of lists after deletion is not list before deletion minus one");
     }
 }
--- src/helpers/CurrentBoardHelper.java
+++ src/helpers/CurrentBoardHelper.java
@@ -1,11 +1,102 @@
 package helpers;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import java.util.List;
 
 public class CurrentBoardHelper extends PageBase{
 
-    public CurrentBoardHelper(WebDriver driver) {
+    public CurrentBoardHelper (WebDriver driver) {
         super(driver);
     }
 
+    // ----- I find index of the last list in Board --------------
+    int numberOfList = countLists();
+
+    //@FindBy (css = ".list-header")
+    //List<WebElement> countListsInBoards;
+
+    //@FindBy (xpath = "//span[@class='placeholder']/..")
+    //WebElement addListButton;
+
+    // @FindBy (name = "name")
+    // WebElement titleListField;
+
+    public int countLists(){
+
+        return driver.findElements(By.cssSelector(".list-header")).size()-1;
+    }
+
+    public void pushButtonAddList(){
+        driver.findElement(By.xpath("//span[@class='placeholder']")).click();
+        waitUntilElementIsClickable(By.xpath("//span[@class='placeholder']/"),10);
+    }
+
+    public void getNameNewList() {
+        WebElement titleListField = driver.findElement(By.xpath("//input[@name = 'name']"));
+        fillField(titleListField, "QWDFKL");
+        driver.findElement(By.xpath("//input[@value = 'Add list']")).click();
+        waitUntilElementIsClickable(By.xpath("//input[@value = 'Add list']"), 10);
+    }
+
+    public void finishAddList(){
+        WebElement cancelEdit = driver.findElement(By.cssSelector(".js-cancel-edit"));
+        cancelEdit.click();
+        waitUntilAllElementsArePresent(By.cssSelector(".list-header"),15);
+    }
+
+    public String checkAvailabilList (){
+        return driver.findElement(By.xpath("//span[@class = 'placeholder']")).getText();
+    }
+
+   public void getNameNewCard() {
+        waitUntilElementIsClickable(By.cssSelector(".open-card-composer"),10);
+        driver.findElement(By.cssSelector(".open-card-composer")).click();
+        waitUntilElementIsClickable(By.cssSelector(".js-card-title"),10);
+        WebElement cardName = driver.findElement(By.cssSelector(".js-card-title"));
+        fillField(cardName, "AddedCard");
+        driver.findElement(By.cssSelector(".js-add-card")).click();
+        waitUntilElementIsClickable(By.cssSelector(".js-cancel"),10);
+        driver.findElement(By.cssSelector(".js-cancel")).click();
+    }
+
+      public int countCard() {
+        return driver.findElements(By.cssSelector(".js-card-details")).size();
+    }
+
+    public void listDelete() {
+        waitUntilElementIsClickable(By.cssSelector(".list-header-extras-menu"),10);
+        driver.findElement(By.cssSelector(".list-header-extras-menu")).click();
+        waitUntilElementIsClickable(By.cssSelector(".js-close-list"),10);
+        driver.findElement(By.cssSelector(".js-close-list")).click();
+
+    }
+
+    public void clickOnHeader() {
+        int lastList = driver.findElements(By.cssSelector(".list-header")).size()-1;
+        WebElement lastHeader = driver.findElements(By.cssSelector(".list-header")).get(lastList);
+        lastHeader.click();
+        waitUntilElementIsClickable(By.cssSelector(".js-list-name-input"),10);
+    }
+
+    public void getNewName() {
+        String newHeader = "anotherHeader1";
+        int lastList = driver.findElements(By.cssSelector(".list-header")).size()-1;
+        WebElement lastNameList = driver.findElements(By.cssSelector(".js-list-name-input"))
+                .get(lastList);
+        lastNameList.sendKeys(newHeader);
+        lastNameList.sendKeys(Keys.ENTER);
+        driver.navigate().refresh();
+        waitUntilAllElementsArePresent(By.cssSelector(".list-header"),15);
+    }
+
+    public String nameOfHeaderList() {
+        int lastList = driver.findElements(By.cssSelector(".list-header")).size()-1;
+        WebElement lastHeader = driver.findElements(By.cssSelector(".list-header")).get(lastList);
+        return lastHeader.getText();
+
+    }
 }
